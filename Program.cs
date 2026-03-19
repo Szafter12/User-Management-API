32c87c50d014ed7ee5f89783f069c27bff4ec603
Jakub Pachut <jakubpachut@gmail.com>
2026-03-20 00:53:48 +0100

    adding swagger

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using User_Management_API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<GlobalExceptionMiddleware>();
 builder.Services.AddTransient<InfoMiddleware>();
 builder.Services.AddTransient<TokenValidationMiddleware>();
@@ -10,6 +12,12 @@ builder.Services.AddTransient<TokenValidationMiddleware>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 app.UseMiddleware<TokenValidationMiddleware>();
