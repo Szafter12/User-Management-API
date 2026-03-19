383009e9c16afb2980b7ea175ad38a8cb2fca46a
Jakub Pachut <jakubpachut@gmail.com>
2026-03-19 21:14:34 +0100

    adding logging middleware

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.Use(
+    async (context, next) =>
+    {
+        Console.WriteLine($"Method: {context.Request.Method} - Path: ${context.Request.Path}");
+        await next.Invoke();
+        Console.WriteLine($"Respone code: {context.Response.StatusCode}");
+    }
+);
 
 app.MapControllers();
 
